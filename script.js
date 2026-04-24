bdf35528bd6ac70b045048caafafbd3e0d50456d
orabi98 <amjad.orabii.98@gmail.com>
2026-04-24 11:32:16 -0400

    Initial Chicago Pro Fix website

---
+++ script.js
@@ -0,0 +1,19 @@
+// Scroll animation
+const animatedElements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+animatedElements.forEach(element => {
+  observer.observe(element);
+});
